d60afdf5447675e434374f6dde508eb0d9b7e81c
iancolombini <45071240+iancolombini@users.noreply.github.com>
2018-11-15 22:07:19 +0700

    Add files via upload

---
+++ app.10.js
@@ -0,0 +1,14 @@
+var Andi = 
+{
+    nama : 'Andi',
+    usia : 50,
+    job : 
+    {
+        nama: 'PNS',
+        tingkat: 'Eselon IIIA',
+        lokasi: 'Bali'
+    }
+};
+
+console.log(Andi.job.nama)
+console.log(Andi.job.lokasi)
